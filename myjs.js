469a99ceee3f841aec5985a101b36cc9a621990c
talupu <talupu@gmail.com>
2016-02-01 15:36:45 +0530

    pointing to javascript code

--- myjs.js
+++ myjs.js
@@ -11,17 +11,6 @@
 			}
 			$("#main-img").attr('src',("img"+counter+".jpg"));
 			} // change pic code ends here
-			
-	setInterval(changePic, 2500); //call function changePic every 2.5 seconds
-	
-//slideshow code ends here
+				
 
 
-//what happens if I click on main image (Today's recipe)
-
-	$("#main-img").click(function(){
-		var imgnum = $("#main-img").attr('src'); // grabbing the image filename
-		var sernum = imgnum.match(/\d+/g); //grabbing the last char which is the number that matches the recipe file sequence
-		$("#main-img-recipe").attr('href', ("recipe" + sernum + ".html")); 	//modifying the href attribute in the link to 
-																			//match the current recipe	
-	});
